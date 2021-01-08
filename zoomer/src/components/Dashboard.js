9330770f7ca1199c16b6d9cae2a82a738110e41b
Christy <appletoms99@outlook.com>
2021-01-07 18:03:15 -0600

    add header, footer

---
+++ zoomer/src/components/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+import '../App.css';
+
+const Dashboard = () => {
+    return (
+        <h1>Dashboard</h1>
+    )
+}
+
+export default Dashboard;
---
+++ zoomer/src/components/SignUp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../App.css';
+
+const SignUp = () => {
+    return (
+        <div className="SignUp">
+
+            <h1>SignUp</h1>
+
+        </div>
+    )
+}
+
+export default SignUp;
